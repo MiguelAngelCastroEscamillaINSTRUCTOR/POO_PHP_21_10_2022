0d10d7983c8442b50bb8246c15224ba192b4bd52
MiguelAngelCastroEscamillaINSTRUCTOR <macastro40@misena.edu.co>
2022-10-24 15:56:06 -0500

    Polimorfismo

--- api.php
+++ api.php
@@ -1,23 +1,51 @@
 <?php
-
-    class a{
-        public $mensaje_A = "Clase a entidad";
-        public function saludo():string{
-            return "Hola soy la clase a :V";
+    class espada{
+        public $material;
+        public $puntos;
+        public $durailidad;
+        public function __construct(
+            string $material="madera",
+            int $puntos=2,
+            int $durailidad=100){
+                $this->material = $material;
+                $this->puntos = $puntos;
+                $this->durailidad = $durailidad;
+        }
+        public function atacar(int $quitar = 4):string{
+            $this->set_durailidad($quitar);
+            return <<<TEXTO
+                Realizo un daño de ${!${''} = $this->puntos} con una espada de ${!${''} = $this->material} ${!${''} = $this->destruye($this->durailidad)}
+            TEXTO;
+        }
+        public function set_durailidad($quitar){
+            $this->durailidad -= $quitar;
+        }
+        static function validacion(string $material):string{
+            return <<<TEXTO2
+                la espada de ${!${''} = $material} se rompio :( <br>
+            TEXTO2;
+        }
+        public function destruye($durabilidad):string{
+            if($durabilidad<=0){
+                $this->puntos = -1;
+                return self::validacion($this->material);
+            }
+            else
+                return "<br>";
         }
     }
-    class b extends a{
-        public $mensaje_b = "Clase b entidad";
-        public function saludo():string{
-            return "Hola soy la clase b :V";
+
+    class diamante extends espada{
+        public function __construct(
+                string $material="diamante",
+                int $puntos=8,
+                int $durailidad=600){
+            $this->material = $material;
+            $this->puntos = $puntos;
+            $this->durailidad = $durailidad;
         }
     }
-    class c extends b{
-        public $mensaje_c = "Clase c entidad";
-    }
-
-    $obj = new c();
-    var_dump($obj->saludo());
-
-
+    $obj = new diamante();
+    echo $obj->atacar(1);
+    var_dump($obj); 
 ?>
